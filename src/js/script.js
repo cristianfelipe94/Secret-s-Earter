5c025c874fa50d50868e4fc051976f6f8cf7d7e4
Cristian Felipe <cristianpipe0494@gmail.com>
2018-12-05 18:26:23 -0600

    Have added Functionality to SecretEater.

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,68 @@
-/**
- * Project Description.
- * ...
- */
+// Get all the Elements from the DOM.
+const secretsBar = document.getElementById('secretsBar');
+const secretsBtn = document.getElementById('secretsBtn');
+const retrieveSecrets = document.getElementById('retrieveSecrets');
+const secretList = document.getElementById('secretList');
 
-const text = 'Hola mundo!';
-const container = document.getElementById('container');
-const paragraph = document.createElement('p');
-paragraph.innerText = text;
-container.appendChild(paragraph);
+// Array where secrets are going to live.
+const secretAnswArray = [];
+
+// Btn will add secrets to list.
+secretsBtn.addEventListener('click', function () {
+  if (secretsBar.value !== '') {
+    localStorage.setItem('newSecret', secretsBar.value);
+    const secretAnsw = localStorage.getItem('newSecret');
+    secretAnswArray.push(secretAnsw);
+    localStorage.setItem('newSecretArray', JSON.stringify(secretAnswArray));
+    const secretAnswArrayResp = JSON.parse(localStorage.getItem('newSecretArray'));
+    console.log(secretAnswArrayResp);
+    secretsBar.value = '';
+  } else {
+    console.log('No information.');
+  }
+});
+
+// This counter works as Index for elements.
+let indexCounter = 0;
+
+// Function will Get and Create elements from Array.
+retrieveSecrets.addEventListener('click', function () {
+
+  // Get the Array and convert it as Json.
+  const secretAnswArrayResp = JSON.parse(localStorage.getItem('newSecretArray'));
+  if (secretAnswArrayResp.length !== '') {
+
+    // For Each element from the Array Do.
+    secretAnswArrayResp.forEach(function(e) {
+
+      // Create elements where information will be store.
+      const secretListItem = document.createElement('li');
+      const secretListBtn = document.createElement('input');
+      const secretListWrapper = document.createElement('div');
+      secretListBtn.setAttribute('type', 'submit')
+
+      // Attribute Data, to create Index Numbers to regular elements.
+      secretListBtn.setAttribute('data-indexOf', indexCounter);
+
+      // Counter will increase Index Number.
+      indexCounter += 1;
+
+      // Add information into de Containers.
+      secretListItem.innerHTML = e;
+      secretListWrapper.appendChild(secretListItem);
+      secretListWrapper.appendChild(secretListBtn);
+      secretList.appendChild(secretListWrapper);
+
+      // Function will live in every Btn.
+      secretListBtn.addEventListener('click', function(e) {
+        const elementIndexNumb = e.target.getAttribute('data-indexOf', indexCounter);
+        console.log(elementIndexNumb);
+        secretAnswArray.splice(elementIndexNumb, 1);
+        localStorage.setItem('newSecretArray', JSON.stringify(secretAnswArray));
+        const secretAnswArrayResp = JSON.parse(localStorage.getItem('newSecretArray'));
+        console.log(secretAnswArrayResp);
+        console.log('removing');
+      });
+    });
+  }
+});
